dfa662606b3b10bcd00bc14bb01d0ac1d81c67f4
bathonSpidey <bathonbhats@gmail.com>
2021-01-07 23:41:14 +0100

    Wrote forward propagate with tests to check dimensions
    At the moment the network is fixed with two activation function will need to adjust each layer with different activation function (Weekend work)

--- DeepNetwork.py
+++ DeepNetwork.py
@@ -9,13 +9,64 @@ import numpy as np
 
 class Network:
     
+    def dataReshape(self,dataset):
+        return dataset.reshape(dataset.shape[0],-1).T
+    
+    def sigmoid(self,linearResult):
+        activatedResult= 1/(1+np.exp(-linearResult))
+        return activatedResult
+    
+    def relu(self, linearResult):
+        activatedResult = np.maximum(0,linearResult)
+        return activatedResult
+    
     def InitializeNetwork(self, layerDimensions):
         parameters={}
         if (len(layerDimensions)==0 or 
-            layerDimensions[0]<=0 or type(layerDimensions) !=list):
+            0 in layerDimensions or type(layerDimensions) !=list):
             raise Exception("Can't initialize network with zero parameters")
         for layer in range(1,len(layerDimensions)):
             parameters["Weights"+str(layer)]=np.random.randn(
                 layerDimensions[layer],layerDimensions[layer-1]) * 0.01
             parameters["bias"+str(layer)]=np.zeros((layerDimensions[layer], 1))
         return parameters
+    
+    def GetLinearResult(self,previousActivation, weights, bias ):
+        linearResult=np.dot(weights,previousActivation)+bias
+        cache=(previousActivation, weights, bias)
+        return linearResult, cache
+    
+    def ActivateLinearResult(self, previousActivation, weight, bias, activation):
+        linearResult, linearCache = self.GetLinearResult(previousActivation,
+                                                          weight, bias)
+        if activation == "sigmoid":
+            activatedResult=self.sigmoid(linearResult)
+        elif activation == "relu":
+           activatedResult=self.relu(linearResult)
+        cache=(linearCache,linearResult)
+        return activatedResult,cache
+    
+    def ForwardPropagate(self,trainData,parameters):
+        caches = []
+        activatedResult = trainData
+        totalLayers=len(parameters) // 2
+        for layer in range(1,totalLayers):
+            previousActivation=activatedResult
+            activatedResult,cache=self.ActivateLinearResult(previousActivation,
+                                                      parameters['Weights'+str(layer)],
+                                                      parameters['bias'+str(layer)], 
+                                                      "relu")
+            caches.append(cache)
+        lastLayerActivation,cache=self.ActivateLinearResult(activatedResult,
+                                                            parameters['Weights'
+                                                                       +str(totalLayers)],
+                                                            parameters['bias'+str(totalLayers)],
+                                                            "sigmoid")
+        caches.append(cache)
+        
+        return lastLayerActivation,caches
+    
+        
+            
+        
+    
--- DeepNetworkTests.py
+++ DeepNetworkTests.py
@@ -6,18 +6,23 @@ Created on Thu Jan  7 09:21:42 2021
 """
 import DeepNetwork
 import unittest
+import numpy as np
 
 class TestNetwork(unittest.TestCase):
+
+    def setUp(self):
+        self.dataset=np.array([(0,0),(0,1),(1,0),(1,1)])
+        self.testDataset=self.dataset
+        self.targetSet=np.array([1,0,0,1])
+        self.network=DeepNetwork.Network()
     
     def test_InitilaizeNetworkWithInvalidInput(self):
-        network=DeepNetwork.Network()
-        with self.assertRaises(Exception):network.InitializeNetwork("1,2,4,3")
-        with self.assertRaises(Exception):network.InitializeNetwork([])
-        with self.assertRaises(Exception):network.InitializeNetwork([0,2,1])
+        with self.assertRaises(Exception):self.network.InitializeNetwork("1,2,4,3")
+        with self.assertRaises(Exception):self.network.InitializeNetwork([])
+        with self.assertRaises(Exception):self.network.InitializeNetwork([0,2,1])
         
     def test_GetParameters(self):
-        network=DeepNetwork.Network()
-        parameters=network.InitializeNetwork([2,2,1])
+        parameters=self.network.InitializeNetwork([2,2,1])
         self.assertEqual(len(parameters.keys()),4)
         self.assertEqual(parameters["Weights1"].shape,(2,2))
         self.assertEqual(parameters["Weights2"].shape,(1,2))
@@ -25,8 +30,7 @@ class TestNetwork(unittest.TestCase):
         self.assertEqual(parameters["bias2"].shape,(1,1))
         
     def test_GetParamtersOfDeeperNetwork(self):
-        network=DeepNetwork.Network()
-        parameters=network.InitializeNetwork([2,8,2,6,1])
+        parameters=self.network.InitializeNetwork([2,8,2,6,1])
         self.assertEqual(len(parameters.keys()),8)
         self.assertEqual(parameters["Weights1"].shape,(8,2))
         self.assertEqual(parameters["Weights2"].shape,(2,8))
@@ -35,10 +39,41 @@ class TestNetwork(unittest.TestCase):
         self.assertEqual(parameters["bias1"].shape,(8,1))
         self.assertEqual(parameters["bias2"].shape,(2,1))
     
+    def test_forwardPropagateForFirstLayerWithOneNode(self):
+        trainData=self.network.dataReshape(self.dataset)
+        parameters=self.network.InitializeNetwork([2,1,1])
+        linearResult,_=self.network.GetLinearResult(trainData,
+                                                         parameters["Weights1"],
+                                                         parameters["bias1"])
+        self.assertEqual(linearResult.shape,(1,4))
     
+    def test_activateFirstLayerWithSigmoid(self):
+        activatedResult,_,_=self.initializeActivationFunction("sigmoid",[2,1,1])
+        self.assertEqual(activatedResult.shape,(1,4))
     
+    def initializeActivationFunction(self, activation, layerList):
+        trainData=self.network.dataReshape(self.dataset)
+        parameters=self.network.InitializeNetwork(layerList)
+        activatedResult,_=self.network.ActivateLinearResult(
+            trainData,parameters["Weights1"],parameters["bias1"], activation)
+        return activatedResult,trainData,parameters
         
-
-
+    def test_activateFirstLayerWithRelu(self):
+        activatedResult,_,_=self.initializeActivationFunction("relu",[2,1,1])
+        self.assertEqual(activatedResult.shape,(1,4))
+    
+    def test_activatedResultForShallowNetwork(self):
+        _,trainData,parameters=self.initializeActivationFunction("relu",[2,1,1])
+        finalLayerActivation,caches=self.network.ForwardPropagate(trainData,
+                                                                  parameters)
+        self.assertEqual(finalLayerActivation.shape,(1,4))
+    
+    def test_activatedResultForDeepNetwork(self):
+        _,trainData,parameters=self.initializeActivationFunction("relu",[2,8,4,2,1])
+        finalLayerActivation,caches=self.network.ForwardPropagate(trainData,
+                                                                  parameters)
+        self.assertEqual(parameters["Weights"+str(len(parameters) // 2)].shape,(1,2))
+        self.assertEqual(finalLayerActivation.shape,(1,4))
+        
 if __name__ == '__main__':
     unittest.main()
