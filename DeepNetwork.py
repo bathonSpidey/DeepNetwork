e501ebe7f5d422defa623ec1f1c8954b5fb30ea9
bathonSpidey <bathonbhats@gmail.com>
2021-01-07 21:02:27 +0100

    Some unsaved changes from morning

--- DeepNetwork.py
+++ DeepNetwork.py
@@ -5,8 +5,6 @@ Created on Tue Jan  5 23:01:33 2021
 @author: batho
 """
 
-import unittest
-
 import numpy as np
 
 class Network:
@@ -21,42 +19,3 @@ class Network:
                 layerDimensions[layer],layerDimensions[layer-1]) * 0.01
             parameters["bias"+str(layer)]=np.zeros((layerDimensions[layer], 1))
         return parameters
-        
-    
-    
-
-class TestNetwork(unittest.TestCase):
-    
-    def test_InitilaizeNetworkWithInvalidInput(self):
-        network=Network()
-        with self.assertRaises(Exception):network.InitializeNetwork("1,2,4,3")
-        with self.assertRaises(Exception):network.InitializeNetwork([])
-        with self.assertRaises(Exception):network.InitializeNetwork([0,2,1])
-        
-    def test_GetParameters(self):
-        network=Network()
-        parameters=network.InitializeNetwork([2,2,1])
-        self.assertEqual(len(parameters.keys()),4)
-        self.assertEqual(parameters["Weights1"].shape,(2,2))
-        self.assertEqual(parameters["Weights2"].shape,(1,2))
-        self.assertEqual(parameters["bias1"].shape,(2,1))
-        self.assertEqual(parameters["bias2"].shape,(1,1))
-        
-    def test_GetParamtersOfDeeperNetwork(self):
-        network=Network()
-        parameters=network.InitializeNetwork([2,8,2,6,1])
-        self.assertEqual(len(parameters.keys()),8)
-        self.assertEqual(parameters["Weights1"].shape,(8,2))
-        self.assertEqual(parameters["Weights2"].shape,(2,8))
-        self.assertEqual(parameters["Weights3"].shape,(6,2))
-        self.assertEqual(parameters["Weights4"].shape,(1,6))
-        self.assertEqual(parameters["bias1"].shape,(8,1))
-        self.assertEqual(parameters["bias2"].shape,(2,1))
-    
-    
-    
-        
-
-
-if __name__ == '__main__':
-    unittest.main()
